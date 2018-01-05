e7d59317837f04e442e4d179ef01477cc499e52b
mkay <mkay@thoughtworks.com>
2018-01-05 14:18:09 +0000

    Mike : Refactor

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var path = require('path');
 
 module.exports = {
-    entry: './client/src/index.js',
+    entry: './client/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
